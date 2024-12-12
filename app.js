c58797b4e2ac589ac6ea31764ccfa3a93305b725
braxtondb <58535509+braxtondb@users.noreply.github.com>
2024-12-12 00:31:31 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,22 +2,62 @@ const cors = require('cors');
 const express = require('express');
 const { MongoClient } = require('mongodb');
 
-async function getUsers(client, args, callback) {
-    const db = client.db('gdl_data');
-    let ret = "";
-    await db.collection('users').find({}).forEach((user) => {ret += ',' + user.id + ',' + user.username});
-    callback(ret.slice(1));
+const uri = 'mongodb+srv://braxtondb:nessmb03@gdll.zfzt0.mongodb.net/?retryWrites=true&w=majority&appName=GDLL';
+const client = new MongoClient(uri);
+let db;
+
+// TODO: Fix datatypes (int)
+
+async function openConnection() {
+    try {
+        await client.connect();
+        db = client.db('gdl_data');
+        console.log('Connected to gdl_data');
+    } catch (e) {
+        console.error('openConnection():', e);
+        process.exit(1);
+    }
 }
 
-async function getRecords(client, args, callback) {
-    const db = client.db('gdl_data');
-    let ret = "";
-    await db.collection('records').find({}).forEach((rec) => {ret += ',' + rec.id + ',' + rec.url + ',' + rec.user.id});
-    callback(ret.slice(1));
+async function closeConnection() {
+    try {
+        await client.close();
+        console.log('Connection closed');
+    } catch (e) {
+        console.error('closeConnection():', e);
+    }
 }
 
-async function addLevel(client, args, callback) {
-    const db = client.db('gdl_data');
+// Query handlers
+
+async function getLevels(args, callback) {
+    let ret = "";
+    const levels = await db.collection('levels').find({}).toArray();
+    for (const lvl of levels) {
+        ret += `,${lvl.id},${lvl.position},${lvl.name},${lvl.publisher.id},${lvl.verification.id},${JSON.stringify(lvl.creators.map(i => i.id)).replaceAll(',','|')},${JSON.stringify(lvl.records.map(i => i.id)).replaceAll(',','|')},${lvl.points},${lvl.enjoyment}`;
+    }
+    await callback(ret.slice(1));
+}
+
+async function getUsers(args, callback) {
+    let ret = "";
+    const users = await db.collection('users').find({}).toArray();
+    for (const user of users) {
+        ret += `,${user.id},${user.username}`;
+    }
+    await callback(ret.slice(1));
+}
+
+async function getRecords(args, callback) {
+    let ret = "";
+    const recs = await db.collection('records').find({}).toArray();
+    for (const rec of recs) {
+        ret += `,${rec.id},${rec.url},${rec.user.id}`;
+    }
+    await callback(ret.slice(1));
+}
+
+async function addLevel(args, callback) {
     await db.collection('levels').insertOne({
         'id': args.id,
         'position': args.position,
@@ -29,11 +69,10 @@ async function addLevel(client, args, callback) {
         'points': args.points,
         'enjoyment': args.enjoyment
     });
-    callback();
+    await callback();
 }
 
-async function editLevel(client, args, callback) {
-    const db = client.db('gdl_data');
+async function editLevel(args, callback) {
     await db.collection('levels').updateOne({'id': args.id}, {$set: {
         'position': args.position,
         'name': args.name,
@@ -44,119 +83,92 @@ async function editLevel(client, args, callback) {
         'points': args.points,
         'enjoyment': args.enjoyment
     }});
-    callback();
+    await callback();
 }
 
-async function deleteLevel(client, args, callback) {
-    const database = client.db('gdl_data');
-    const levels = database.collection('levels');
+async function deleteLevel(args, callback) {
+    const levels = db.collection('levels');
     await levels.deleteOne({'id': args.id});
-    callback();
+    await callback();
 }
 
-async function addUser(client, args, callback) {
-    const db = client.db('gdl_data');
+async function addUser(args, callback) {
     await db.collection('users').insertOne({'id': args.id, 'username': args.username});
-    callback();
+    await callback();
 }
 
-async function editUser(client, args, callback) {
-    const db = client.db('gdl_data');
+async function editUser(args, callback) {
     await db.collection('users').updateOne({'id': args.id}, {$set: {'username': args.username}});
-    callback();
+    await callback();
 }
 
-async function deleteUser(client, args, callback) {
-    const db = client.db('gdl_data');
+async function deleteUser(args, callback) {
     await db.collection('users').deleteOne({'id': args.id});
-    callback();
+    await callback();
 }
 
-async function addRecord(client, args, callback) {
-    const db = client.db('gdl_data');
-    let user = await db.collection('users').findOne({'id': args.userid});
+async function addRecord(args, callback) {
+    const user = await db.collection('users').findOne({'id': args.userid});
     await db.collection('records').insertOne({'id': args.id, 'url': args.url, 'user': user});
-    callback();
+    await callback();
 }
 
-async function editRecord(client, args, callback) {
-    const db = client.db('gdl_data');
-    let user = await db.collection('users').findOne({'id': args.userid});
+async function editRecord(args, callback) {
+    const user = await db.collection('users').findOne({'id': args.userid});
     await db.collection('records').updateOne({'id': args.id}, {$set: {'url': args.url, 'user': user}});
-    callback();
+    await callback();
 }
 
-async function deleteRecord(client, args, callback) {
-    const db = client.db('gdl_data');
+async function deleteRecord(args, callback) {
     await db.collection('records').deleteOne({'id': args.id});
-    callback();
+    await callback();
 }
+
+// Initialize app
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-app.get('/getusers', (req, res) => {
-    query(getUsers, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
+function registerQuery(query, handler) {
+    app.get(query, async (req, res) => {
+        await handler(req.query, async (val)=>{ res.send(val) }).catch(console.dir);
+        console.log('Response sent to ' + query);
+    });
+}
 
-app.get('/getrecords', (req, res) => {
-    query(getRecords, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
-
-app.get('/addlevel', (req, res) => {
-    query(addLevel, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
-
-app.get('/editlevel', (req, res) => {
-    query(editLevel, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
-
-app.get('/deletelevel', (req, res) => {
-    query(deleteLevel, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
-
-app.get('/adduser', (req, res) => {
-    query(addUser, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
-
-app.get('/edituser', (req, res) => {
-    query(editUser, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
-
-app.get('/deleteuser', (req, res) => {
-    query(deleteUser, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
-
-app.get('/addrecord', (req, res) => {
-    query(addRecord, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
-
-app.get('/editrecord', (req, res) => {
-    query(editRecord, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
-
-app.get('/deleterecord', (req, res) => {
-    query(deleteRecord, req.query, (val) => {res.send(val)}).catch(console.dir);
-});
+registerQuery('/getlevels',    getLevels);
+registerQuery('/getusers',     getUsers);
+registerQuery('/getrecords',   getRecords);
+registerQuery('/addlevel',     addLevel);
+registerQuery('/editlevel',    editLevel);
+registerQuery('/deletelevel',  deleteLevel);
+registerQuery('/adduser',      addUser);
+registerQuery('/edituser',     editUser);
+registerQuery('/deleteuser',   deleteUser);
+registerQuery('/addrecord',    addRecord);
+registerQuery('/editrecord',   editRecord);
+registerQuery('/deleterecord', deleteRecord);
 
 app.listen(port, () => {
   console.log(`Web server listening on port ${port}`);
 });
 
+// App exit
+
+process.on('SIGINT', async () => {
+    await closeConnection();
+    process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+    await closeConnection();
+    process.exit(0);
+});
+
 // Connect to Mongo cluster
 
-const uri = 'mongodb+srv://braxtondb:nessmb03@gdll.zfzt0.mongodb.net/?retryWrites=true&w=majority&appName=GDLL';
-
-const client = new MongoClient(uri);
-
-async function query(myquery, args, callback) {
-    try {
-        await client.connect();
-        await myquery(client, args, callback);
-    } finally {
-        console.log("Sending response");
-        await client.close();
-    }
-}
+(async () => {
+    await openConnection();
+})();
--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 // let users = [];
 // let records = [];
 
+// TODO: Input format checking and illegal char (comma) removal
+// TODO: const everywhere
+// maybe enter url or userid instead of dropdown to auto create if necessary (don't do this, need dropdown points)
+// refresh button
+
+let currentTab = 'levels';
+
+class Level {
+    constructor(id, position, name, publisherID, verificationID, creatorIDs, recordIDs, points, enjoyment) {
+        this.id = Number(id);
+        this.position = Number(position);
+        this.name = name;
+        this.publisherID = Number(publisherID);
+        this.verificationID = Number(verificationID);
+        this.creatorIDs = creatorIDs.map(i => Number(i));
+        this.recordIDs = recordIDs.map(i => Number(i));
+        this.points = Number(points);
+        this.enjoyment = Number(enjoyment);
+    }
+}
+
+class User {
+    constructor(id, username) {
+        this.id = Number(id);
+        this.username = username;
+    }
+}
+
+class Record {
+    constructor(id, url, userID) {
+        this.id = Number(id);
+        this.url = url;
+        this.userID = Number(userID);
+    }
+}
+
 function httpGetAsync(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
@@ -9,6 +45,49 @@ function httpGetAsync(theUrl, callback) {
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
+}
+
+function getLevels(callback) {
+    httpGetAsync(`http://localhost:3000/getlevels`, callback);
+}
+
+function getUsers(callback) {
+    httpGetAsync(`http://localhost:3000/getusers`, callback);
+}
+
+function getRecords(callback) {
+    httpGetAsync(`http://localhost:3000/getrecords`, callback);
+}
+
+function responseToLevels(response) {
+    const data = response.split(',');
+    let levels = [];
+    for (let i = 0; i < data.length; i += 9) {
+        levels.push(new Level(data[i], data[i+1], data[i+2], data[i+3], data[i+4], (data[i+5] || '').split('|'), (data[i+6] || '').split('|'), data[i+7], data[i+8]));
+    }
+    return levels;
+}
+
+function responseToUsers(response) {
+    const data = response.split(',');
+    let users = [];
+    for (let i = 0; i < data.length; i += 2) {
+        users.push(new User(data[i], data[i+1]));
+    }
+    return users;
+}
+
+function responseToRecords(response) {
+    const data = response.split(',');
+    let records = [];
+    for (let i = 0; i < data.length; i += 3) {
+        records.push(new Record(data[i], data[i+1], data[i+2]));
+    }
+    return records;
+}
+
+function findID(list, id) {
+    return list.find(i => i.id == id);
 }
 
 function addLevel() {
@@ -26,7 +105,7 @@ function addLevel() {
     creators = creators.slice(1);
     records = records.slice(1);
 
-    httpGetAsync(`http://localhost:3000/addlevel?id=${id}&position=${position}&name=${name}&publisher=${publisher}&verification=${verification}&creators=${creators}&records=${records}&points=${points}&enjoyment=${enjoyment}`, function(response) {});
+    httpGetAsync(`http://localhost:3000/addlevel?id=${id}&position=${position}&name=${name}&publisher=${publisher}&verification=${verification}&creators=${creators}&records=${records}&points=${points}&enjoyment=${enjoyment}`, function(response) {sortLevels()});
 
     $('#addlevel input').val('');
     $('#backdrop').css('display', 'none');
@@ -47,7 +126,7 @@ function editLevel() {
     creators = creators.slice(1);
     records = records.slice(1);
 
-    httpGetAsync(`http://localhost:3000/editlevel?id=${id}&position=${position}&name=${name}&publisher=${publisher}&verification=${verification}&creators=${creators}&records=${records}&points=${points}&enjoyment=${enjoyment}`, function(response) {});
+    httpGetAsync(`http://localhost:3000/editlevel?id=${id}&position=${position}&name=${name}&publisher=${publisher}&verification=${verification}&creators=${creators}&records=${records}&points=${points}&enjoyment=${enjoyment}`, function(response) {sortLevels()});
 
     $('#editlevel input').val('');
     $('#backdrop').css('display', 'none');
@@ -56,7 +135,7 @@ function editLevel() {
 function deleteLevel() {
     let id = $('#deletelevel input')[0].value;
 
-    httpGetAsync(`http://localhost:3000/deletelevel?id=${id}`, function(response) {});
+    httpGetAsync(`http://localhost:3000/deletelevel?id=${id}`, function(response) {sortLevels()});
 
     $('#deletelevel input').val('');
     $('#backdrop').css('display', 'none');
@@ -66,7 +145,7 @@ function addUser() {
     let id = $('#adduser input')[0].value;
     let username = $('#adduser input')[1].value;
 
-    httpGetAsync(`http://localhost:3000/adduser?id=${id}&username=${username}`, function(response) {});
+    httpGetAsync(`http://localhost:3000/adduser?id=${id}&username=${username}`, function(response) {updateUserTable()});
 
     $('#adduser input').val('');
     $('#backdrop').css('display', 'none');
@@ -76,7 +155,7 @@ function editUser() {
     let id = $('#edituser input')[0].value;
     let username = $('#edituser input')[1].value;
 
-    httpGetAsync(`http://localhost:3000/edituser?id=${id}&username=${username}`, function(response) {});
+    httpGetAsync(`http://localhost:3000/edituser?id=${id}&username=${username}`, function(response) {updateUserTable()});
 
     $('#edituser input').val('');
     $('#backdrop').css('display', 'none');
@@ -85,7 +164,7 @@ function editUser() {
 function deleteUser() {
     let id = $('#deleteuser input')[0].value;
 
-    httpGetAsync(`http://localhost:3000/deleteuser?id=${id}`, function(response) {});
+    httpGetAsync(`http://localhost:3000/deleteuser?id=${id}`, function(response) {updateUserTable()});
 
     $('#deleteuser input').val('');
     $('#backdrop').css('display', 'none');
@@ -96,7 +175,7 @@ function addRecord() {
     let url = $('#addrecord input')[1].value;
     let userid = $('#addrecord input')[2].value;
 
-    httpGetAsync(`http://localhost:3000/addrecord?id=${id}&url=${url}&userid=${userid}`, function(response) {});
+    httpGetAsync(`http://localhost:3000/addrecord?id=${id}&url=${url}&userid=${userid}`, function(response) {updateRecordTable()});
 
     $('#addrecord input').val('');
     $('#backdrop').css('display', 'none');
@@ -107,7 +186,7 @@ function editRecord() {
     let url = $('#editrecord input')[1].value;
     let userid = $('#editrecord input')[2].value;
 
-    httpGetAsync(`http://localhost:3000/editrecord?id=${id}&url=${url}&userid=${userid}`, function(response) {});
+    httpGetAsync(`http://localhost:3000/editrecord?id=${id}&url=${url}&userid=${userid}`, function(response) {updateRecordTable()});
 
     $('#editrecord input').val('');
     $('#backdrop').css('display', 'none');
@@ -116,13 +195,14 @@ function editRecord() {
 function deleteRecord() {
     let id = $('#deleterecord input')[0].value;
 
-    httpGetAsync(`http://localhost:3000/deleterecord?id=${id}`, function(response) {});
+    httpGetAsync(`http://localhost:3000/deleterecord?id=${id}`, function(response) {updateRecordTable()});
 
     $('#deleterecord input').val('');
     $('#backdrop').css('display', 'none');
 }
 
 function openTab(tabid) {
+    currentTab = tabid;
     $('.tabcont').css('display', 'none');
     $(`#${tabid}cont`).css('display', 'block');
     $('.tabbutton').removeClass('active');
@@ -131,48 +211,181 @@ function openTab(tabid) {
 
 function openMenu(menuid) {
     $('#backdrop div').css('display', 'none');
-    $(`#${menuid}`).css('display', '');
     $(`#backdrop`).css('display', '');
+    
+    if (menuid == 'sortmenu') {
+        menuid = 'sort' + currentTab;
+        $(`#${menuid}`).css('display', '');
+        return;
+    }
+
+    $(`#${menuid}`).css('display', '');
     checkInput(menuid);
 
-    switch (menuid) {
-        case 'addlevel':
-            $('#addlevel select').addClass('incomplete');
-            $("#addlevel option[value!='']").each((i, el) => { $(el).remove(); });
-            httpGetAsync(`http://localhost:3000/getusers`, function(response) {
-                let data = response.split(',');
-                for (let i = 0; i < data.length; i += 2) {
-                    $('#addlevel .userdrop.incomplete').append($('<option>', { value: data[i], text: `${data[i+1]} (user_id: ${data[i]})` }));
-                }
-                $('#addlevel .userdrop.incomplete').removeClass('.incomplete');
-            });
-            httpGetAsync(`http://localhost:3000/getrecords`, function(response) {
-                let data = response.split(',');
-                for (let i = 0; i < data.length; i += 3) {
-                    $('#addlevel .recorddrop.incomplete').append($('<option>', { value: data[i], text: `${data[i+1]} (rec_id: ${data[i]})` }));
-                }
-                $('#addlevel .recorddrop.incomplete').removeClass('.incomplete');
-            });
-            break;
-        case 'editlevel':
-            $('#editlevel select').addClass('incomplete');
-            $("#editlevel option[value!='']").each((i, el) => { $(el).remove(); });
-            httpGetAsync(`http://localhost:3000/getusers`, function(response) {
-                let data = response.split(',');
-                for (let i = 0; i < data.length; i += 2) {
-                    $('#editlevel .userdrop.incomplete').append($('<option>', { value: data[i], text: `${data[i+1]} (user_id: ${data[i]})` }));
-                }
-                $('#editlevel .userdrop.incomplete').removeClass('.incomplete');
-            });
-            httpGetAsync(`http://localhost:3000/getrecords`, function(response) {
-                let data = response.split(',');
-                for (let i = 0; i < data.length; i += 3) {
-                    $('#editlevel .recorddrop.incomplete').append($('<option>', { value: data[i], text: `${data[i+1]} (rec_id: ${data[i]})` }));
-                }
-                $('#editlevel .recorddrop.incomplete').removeClass('.incomplete');
-            });
-            break;
+    getUsers(response => {
+        let users = responseToUsers(response);
+
+        getRecords(response => {
+            let records = responseToRecords(response);
+
+            switch (menuid) {
+                case 'addlevel':
+                    $('#addlevel select').addClass('incomplete');
+                    $("#addlevel option[value!='']").each((i, el) => { $(el).remove(); });
+
+                    $('#addlevel .userdrop.incomplete').removeClass('.incomplete');
+                    for (const usr of users) {
+                        $('#addlevel .userdrop.incomplete').append($('<option>', { value: usr.id, text: `${usr.username} (id: ${usr.id})` }));
+                    }
+
+                    $('#addlevel .recorddrop.incomplete').removeClass('.incomplete');
+                    for (const rec of records) {
+                        $('#addlevel .recorddrop.incomplete').append($('<option>', { value: rec.id, text: `${rec.url} (by ${(findID(users, rec.userID) || {username:'N/A'}).username})` }));
+                    }
+                    break;
+
+                case 'editlevel':
+                    $('#editlevel select').addClass('incomplete');
+                    $("#editlevel option[value!='']").each((i, el) => { $(el).remove(); });
+
+                    $('#editlevel .userdrop.incomplete').removeClass('.incomplete');
+                    for (const usr of users) {
+                        $('#editlevel .userdrop.incomplete').append($('<option>', { value: usr.id, text: `${usr.username} (id: ${usr.id})` }));
+                    }
+
+                    $('#editlevel .recorddrop.incomplete').removeClass('.incomplete');
+                    for (const rec of records) {
+                        $('#editlevel .recorddrop.incomplete').append($('<option>', { value: rec.id, text: `${rec.url} (by ${(findID(users, rec.userID) || {username:'N/A'}).username})` }));
+                    }
+                    break;
+            }
+        });
+    });
+}
+
+function updateLevelTable(users, records, levels) {
+    $("#datalist table tr").each((i, el) => { $(el).remove(); });
+    $("#datalist table").append('<tr><th>#</th><th>Name</th><th>Verifier</th><th>ID</th>><th>Points</th><th>Rating</th></tr>');
+
+    for (const lvl of levels) {
+        $("#datalist table").append(`<tr><td>${lvl.position}</td><td>${lvl.name}</td><td>${(findID(users, (findID(records, lvl.verificationID) || {userID:-Infinity}).userID) || {username:'N/A'}).username}</td><td>${lvl.id}</td><td>${Math.floor(lvl.points * 10) / 10 || 'N/A'}</td><td>${lvl.enjoyment ? Math.round(lvl.enjoyment) / 10 + '/10' : 'N/A'}</td></tr>`);
     }
+}
+
+function updateUserTable(users) {
+    $("#datalist table tr").each((i, el) => { $(el).remove(); });
+    $("#datalist table").append('<tr><th>ID</th><th>Username</th>');
+
+    for (const usr of users) {
+        $("#datalist table").append(`<tr><td>${usr.id}</td><td>${usr.username}</td>`);
+    }
+}
+
+function updateRecordTable(users, records) {
+    $("#datalist table tr").each((i, el) => { $(el).remove(); });
+    $("#datalist table").append('<tr><th>ID</th><th>Url</th><th>Uploader</th>');
+
+    for (const rec of records) {
+        $("#datalist table").append(`<tr><td>${rec.id}</td><td>${rec.url}</td><td>${(findID(users, rec.userID) || {username:'N/A'}).username}</td>`);
+    }
+}
+
+function sortLevels() {
+    let type = $('#lvlsort')[0].value;
+    let asc = $('#lvlsortsw')[0].checked;
+    let mod = asc ? 1 : -1;
+    $('#backdrop').css('display', 'none');
+
+    getUsers(response => {
+        let users = responseToUsers(response);
+
+        getRecords(response => {
+            let records = responseToRecords(response);
+
+            getLevels(response => {
+                let levels = responseToLevels(response);
+                switch (type) {
+                    case 'position':
+                        levels = levels.sort((a, b) => (a.position < b.position ? -1 : 1) * mod);
+                        break;
+                    case 'name':
+                        levels = levels.sort((a, b) => (a.name.localeCompare(b.name) ? a.name.localeCompare(b.name) : (a.position < b.position ? -1 : 1)) * mod);
+                        break;
+                    case 'verifier':
+                        levels = levels.sort((a, b) => {let cmp = (findID(users, (findID(records, a.verificationID) || {userID:-Infinity}).userID) || {username:'N/A'}).username.localeCompare((findID(users, (findID(records, b.verificationID) || {userID:-Infinity}).userID) || {username:'N/A'}).username); return (cmp ? cmp : (a.position < b.position ? -1 : 1)) * mod; });
+                        break;
+                    case 'id':
+                        levels = levels.sort((a, b) => (a.id < b.id ? -1 : 1) * mod);
+                        break;
+                    case 'points':
+                        levels = levels.sort((a, b) => ((Number(a.points) || 0) < (Number(b.points) || 0) ? -1 : ((Number(a.points) || 0) > (Number(b.points) || 0) ? 1 : (a.position < b.position ? -1 : 1))) * mod);
+                        break;
+                    case 'enjoyment':
+                        levels = levels.sort((a, b) => ((Number(a.enjoyment) || 0) < (Number(b.enjoyment) || 0) ? -1 : ((Number(a.enjoyment) || 0) > (Number(b.enjoyment) || 0) ? 1 : (a.position < b.position ? -1 : 1))) * mod);
+                        break;
+                    case 'publisher':
+                        levels = levels.sort((a, b) => {let cmp = (findID(users, a.publisherID) || {username:'N/A'}).username.localeCompare((findID(users, b.publisherID) || {username:'N/A'}).username); return (cmp ? cmp : (a.position < b.position ? -1 : 1)) * mod; });
+                        break;
+                    case 'creator':
+                        levels = levels.sort((a, b) => {let cmp = (findID(users, a.creatorIDs[0]) || {username:'N/A'}).username.localeCompare((findID(users, b.creatorIDs[0]) || {username:'N/A'}).username); return (cmp ? cmp : (a.position < b.position ? -1 : 1)) * mod; });
+                        break;
+                    case 'records':
+                        levels = levels.sort((a, b) => {let cmp = (findID(users, (findID(records, a.recordIDs[0]) || {userID:-Infinity}).userID) || {username:'N/A'}).username.localeCompare((findID(users, (findID(records, b.recordIDs[0]) || {userID:-Infinity}).userID) || {username:'N/A'}).username); return (cmp ? cmp : (a.position < b.position ? -1 : 1)) * mod; });
+                        break;
+                }
+                updateLevelTable(users, records, levels);
+            });
+        });
+    });
+}
+
+function sortUsers() {
+    let type = $('#usrsort')[0].value;
+    let asc = $('#usrsortsw')[0].checked;
+    let mod = asc ? 1 : -1;
+    $('#backdrop').css('display', 'none');
+
+    getUsers(response => {
+        let users = responseToUsers(response);
+
+        switch (type) {
+            case 'id':
+                users = users.sort((a, b) => (a.id < b.id ? -1 : 1) * mod);
+                break;
+            case 'username':
+                users = users.sort((a, b) => (a.username.localeCompare(b.username) ? a.username.localeCompare(b.username) : (a.id < b.id ? -1 : 1)) * mod);
+                break;
+        }
+        updateUserTable(users);
+    });
+}
+
+function sortRecords() {
+    let type = $('#recsort')[0].value;
+    let asc = $('#recsortsw')[0].checked;
+    let mod = asc ? 1 : -1;
+    $('#backdrop').css('display', 'none');
+
+    getUsers(response => {
+        let users = responseToUsers(response);
+
+        getRecords(response => {
+            let records = responseToRecords(response);
+
+            switch (type) {
+                case 'id':
+                    records = records.sort((a, b) => (a.id < b.id ? -1 : 1) * mod);
+                    break;
+                case 'url':
+                    records = records.sort((a, b) => (a.url.localeCompare(b.url) ? a.url.localeCompare(b.url) : (a.id < b.id ? -1 : 1)) * mod);
+                    break;
+                case 'username':
+                    records = records.sort((a, b) => {let cmp = (findID(users, a.userID) || {username:'N/A'}).username.localeCompare((findID(users, b.userID) || {username:'N/A'}).username); return (cmp ? cmp : (a.id < b.id ? -1 : 1)) * mod; });
+                    break;
+            }
+            updateRecordTable(users, records);
+        });
+    });
 }
 
 function checkInput(menuid) {
@@ -197,6 +410,8 @@ function checkInput(menuid) {
 
 window.onload = function() {
     openTab('levels');
-    $('#backdrop div').click(function(event) {event.preventDefault(); event.stopPropagation()});
     $('#backdrop').css('display', 'none');
+    sortLevels();
+    $('#backdrop').click(function(event) {if (!$(event.target).closest('div').length) { $(this).css('display', 'none'); } });
+    $('#backdrop div').click(function(event) { event.stopPropagation(); });
 }
